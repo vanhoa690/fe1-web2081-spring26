030fe10aad4a9b28839b573eb767998b9909d01a
vanhoa690 <vanhoa690@gmail.com>
2026-03-12 12:43:54 +0700

    contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   {
     path: 'home', // ko code /home
     component: Home,
+  },
+  {
+    path: 'contact',
+    component: Contact,
   },
 ];
