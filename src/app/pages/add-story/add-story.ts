50306252a3291e5790f90a916940fabfb0c4b47b
vanhoa690 <vanhoa690@gmail.com>
2026-03-17 12:24:05 +0700

    khoi tao form anngular

---
+++ src/app/pages/add-story/add-story.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-story',
+  imports: [ReactiveFormsModule], // import module form
+  templateUrl: './add-story.html',
+  styleUrl: './add-story.css',
+})
+export class AddStory {
+  addForm: FormGroup; // ten bien addForm
+
+  constructor(private fb: FormBuilder) {
+    this.addForm = this.fb.group({
+      title: '', // form co input title
+    });
+  }
+}
