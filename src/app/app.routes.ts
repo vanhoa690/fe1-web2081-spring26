f463b5cc6eb1bb19c46e5a93cbac0df6e4469887
vanhoa690 <vanhoa690@gmail.com>
2026-04-02 08:26:17 +0700

    update base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,9 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Contact } from './pages/contact/contact';
-import { Products } from './pages/products/products';
-import { About } from './pages/about/about';
 import { Stories } from './pages/stories/stories';
 import { AddStory } from './pages/add-story/add-story';
 import { EditStory } from './pages/edit-story/edit-story';
 
 export const routes: Routes = [
-  {
-    path: 'home', // ko code /home
-    component: Home,
-  },
-  {
-    path: 'contact',
-    component: Contact,
-  },
-  {
-    path: 'about',
-    component: About,
-  },
-  {
-    path: 'products',
-    component: Products,
-  },
   {
     path: 'stories',
     component: Stories,
